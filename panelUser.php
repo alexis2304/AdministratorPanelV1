92f4f602392bd218ad981a3c04b06615f5485ca6
Alexis Gougaut <lostrom2011@gmail.com>
2016-11-19 00:08:06 +0100

    update v1.0

---
+++ panelUser.php
@@ -0,0 +1,86 @@
+<?php include('config.php'); ?>
+
+<?php 
+
+if(isset($_GET['type']) && isset($_GET['id'])){
+    $type = htmlspecialchars($_GET['type']);
+    $id = htmlspecialchars($_GET['id']);
+    
+    if($type == "delete"){
+        $sql = "DELETE FROM escrow WHERE id=" . $id;
+
+        if ($conn->query($sql) === TRUE) {
+            header('location:panelUser.php');
+        } else {
+            header('location:panelUser.php');
+        }
+    }
+}
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>Hello World!</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+    <body>
+        <div class="bod">
+            <div class="MenuLeft">
+            <ul>
+                <a href="panelEscrow.php">Gestion de l'Escrow</a>
+                <a href="panelUser.php">Gestion des utilisateurs</a>
+                <div class="bottom">
+                    <p></p>
+                </div>
+                
+            </ul>
+            </div>
+            <div class="MenuCenter">
+                <h1>Gestion des utilisateurs</h1>
+                <p>Gestions de tous les utilisateurs dans la base de données.</p>
+                <p>( EN COUR DE CONSTRUCTION )</p>
+                <br /><br />
+                 
+                <?php 
+                $sql = "SELECT * FROM users";
+                $result = $conn->query($sql);
+
+                if ($result->num_rows > 0) {
+                    // output data of each row
+                    while($row = $result->fetch_assoc()) {
+                        $data = json_decode($row['data_escrow']);
+                        echo '<div class="commande">';
+                        echo '<p>Type <b>'. $data->{'type_vendeur'} .'</b></p>';
+                        echo '<p>Grade <b>'. $data->{'grade_vendeur'} .'</b></p>';
+                        echo '<a href="#" class="button">Voir plus de details</a>';
+                        echo '<a href="#" class="button">Supprimer</a>';
+                        echo '</div>';
+                    }
+                } else {
+                    echo "<p>Il n'y a aucune commande sous escrow.</p>";
+                }
+                $conn->close();
+                ?>
+                <!--
+                <div class="commande">
+                    <h1>Huston</h1><br />
+                    <p>Type <b>Admin</b></p>
+                    <p>Grade <b>Débutant</b></p><br />
+                    
+                </div>
+                -->
+            </div>
+        </div>
+    </body>
+    <div class="foot2">
+    <p style="font-size:11px;text-align:center;">Design & Code by Huston</p>
+  </div>
+  <script>
+    // You can also require other files to run in this process
+    require('./renderer.js')
+  </script>
+    
+</html>
+
